f6dd7b67f50c7281a85b1730c579b55355a04a1b
kamiyabali123 <52094486+kamiyabali123@users.noreply.github.com>
2019-09-06 23:00:08 +0530

    mongodb insertion

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://sandvic1:M4f6Tvhdh8QhDvKK@opensaucecluster-qfoum.mongodb.net/test?retryWrites=true&w=majority";
+
+
 app.use(express.json());
 
 app.get('/', function(req, res) {
@@ -17,30 +21,19 @@ app.post('/check', function(req, res) {
     res.send({'message': 'connected!' });
 });
 app.post('/insert',function(req,res) {
-    console.log(req.body);
-    res.send({'message': 'connected!' });
-    // req.on('data',function(data){
-    //     obj = JSON.parse(data);
-    //     console.log(obj.name);
-    // });
     //console.log(req.body);
-    // const MongoClient = require('mongodb').MongoClient;
-    // const uri = "mongodb+srv://sandvic1:M4f6Tvhdh8QhDvKK@opensaucecluster-qfoum.mongodb.net/test?retryWrites=true&w=majority";
-    // const client = new MongoClient(uri, { useNewUrlParser: true });
-    // MongoClient.connect(uri, function(err, client) {
-    //     if(err) {
-    //         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
-    //     }
-    //     console.log('Connected...');
-    //     const collection = client.db("trial_db").collection("trial_collection");
-    //     // perform actions on the collection object
-    //     // var sample={name:"Kamiyabali"};
-    //     // collection.insertOne(sample,function(err,res){
-    //     //     if(err) throw err;
-    //     //     console.log("inserted");
-    //     // })
-    //
-    //     client.close();
-    // });
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+     MongoClient.connect(uri, function(err, client) {
+         if(err) {
+             console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+         }
+         const collection = client.db("trial_db").collection("trial_collection");
+          collection.insertOne(req.body,function(err,res){
+              if(err) throw err;
+              console.log("inserted");
+          });
+
+         client.close();
+     });
 
 });
