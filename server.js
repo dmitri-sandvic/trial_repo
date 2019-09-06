22b5d905fe4afe351b5ea50d5bbb01365afcb64c
kamiyabali123 <52094486+kamiyabali123@users.noreply.github.com>
2019-09-06 11:49:38 +0530

    method insert

--- server.js
+++ server.js
@@ -15,3 +15,25 @@ app.listen(process.env.PORT || 4000, function(){
 app.post('/check', function(req, res) {
     res.send({'message': 'connected!' });
 });
+app.post('/insert',function(req,res) {
+    console.log(req.data);
+    // const MongoClient = require('mongodb').MongoClient;
+    // const uri = "mongodb+srv://sandvic1:M4f6Tvhdh8QhDvKK@opensaucecluster-qfoum.mongodb.net/test?retryWrites=true&w=majority";
+    // const client = new MongoClient(uri, { useNewUrlParser: true });
+    // MongoClient.connect(uri, function(err, client) {
+    //     if(err) {
+    //         console.log('Error occurred while connecting to MongoDB Atlas...\n',err);
+    //     }
+    //     console.log('Connected...');
+    //     const collection = client.db("trial_db").collection("trial_collection");
+    //     // perform actions on the collection object
+    //     // var sample={name:"Kamiyabali"};
+    //     // collection.insertOne(sample,function(err,res){
+    //     //     if(err) throw err;
+    //     //     console.log("inserted");
+    //     // })
+    //
+    //     client.close();
+    // });
+
+});
